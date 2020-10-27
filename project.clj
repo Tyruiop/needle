038b90fdd70e135d4b9aa9ef409296ed9da0da8c
Tyruiop <archibald.pontier+git@tyruiop.org>
2020-10-27 14:01:44 +0800

    Updated project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject needle "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple Clojure profiler based on chrome://tracing"
+  :url "https://github.com/Tyruiop/needle"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
