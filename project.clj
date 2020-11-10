216bc12dcd5aa9549085e66cc17a9ec1488ca320
Tyruiop <archibald.pontier+git@tyruiop.org>
2020-11-10 12:30:45 +0800

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject needle "0.1.4"
+(defproject needle "0.1.5"
   :description "A simple Clojure profiler based on chrome://tracing"
   :url "https://github.com/Tyruiop/needle"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
